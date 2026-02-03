256edb30f3f98309f0e498d33c57c6d4e98988bb
Rafael Missio <rtmissio@gmail.com>
2026-02-03 08:37:33 -0300

    adding the initial project structure

---
+++ src/test/java/br/com/missio/dseventos/DseventosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.missio.dseventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DseventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
